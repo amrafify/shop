ba1af99a5922d8b4a3884b48b9c9e1268026e38b
amrafify <amraffiy@gmail.com>
2024-03-03 21:48:30 +0200

    e

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -87,7 +87,7 @@ export function CartContextProvid({ children }) {
       shippingAddress: data,
     };
     return axios.post(
-      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:3000`,
+      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=https://amrafify.github.io/shop/#`,
       body,
       option
     );
